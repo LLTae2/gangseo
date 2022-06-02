0ee5679f221ea9b0468a553c1a3ee19b1a33377d
taehyun7262 <taehyun5820@gmail.com>
2022-06-02 21:05:42 +0900

    지도 중간에 마커 한 개 띄움

--- src/components/kakaomapscript.js
+++ src/components/kakaomapscript.js
@@ -2,9 +2,23 @@ const { kakao } = window;
 
 export default function KakaoMapScript() {
     const container = document.getElementById('myMap');
+
     const options = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        center: new kakao.maps.LatLng(35.146658, 128.824483),
         level: 3
     };
     const map = new kakao.maps.Map(container, options);
+
+    let markerPosition = new kakao.maps.LatLng(
+        35.146658,
+        128.824483
+      ); //생성될 마커의 위도 경도를 설정
+  
+      // 마커를 생성
+      let marker = new kakao.maps.Marker({
+        position: markerPosition,   
+      });
+  
+      // 마커를 지도 위에 표시
+      marker.setMap(map);
 }  
