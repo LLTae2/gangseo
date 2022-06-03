0307a052c3a634f489a4bd5b4746f6e277ef6b8a
taehyun7262 <taehyun5820@gmail.com>
2022-06-03 18:48:57 +0900

    임의 데이터를 이용해서 마커 여러개를 띄움

--- src/components/kakaomapscript.js
+++ src/components/kakaomapscript.js
@@ -1,24 +1,36 @@
+import { markerdata } from "./markerData";
+
 const { kakao } = window;
 
 export default function KakaoMapScript() {
     const container = document.getElementById('myMap');
 
     const options = {
-        center: new kakao.maps.LatLng(35.146658, 128.824483),
+        center: new kakao.maps.LatLng(35.150369020443875, 128.87847858651804),
         level: 3
     };
     const map = new kakao.maps.Map(container, options);
 
-    let markerPosition = new kakao.maps.LatLng(
-        35.146658,
-        128.824483
-      ); //생성될 마커의 위도 경도를 설정
+    // let markerPosition = new kakao.maps.LatLng(
+    //     35.150369020443875,
+    //     128.87847858651804
+    //   ); //생성될 마커의 위도 경도를 설정
   
-      // 마커를 생성
-      let marker = new kakao.maps.Marker({
-        position: markerPosition,   
+    //   // 마커를 생성
+    //   let marker = new kakao.maps.Marker({
+    //     position: markerPosition,   
+    //   });
+  
+    //   // 마커를 지도 위에 표시
+    //   marker.setMap(map);
+
+    markerdata.forEach((el) => {
+      //마커를 생성함
+      new kakao.maps.Marker({
+        //마커가 표시 될 지도
+        map: map,
+        position: new kakao.maps.LatLng(el.lat, el.lng),
+        title: el.title,
       });
-  
-      // 마커를 지도 위에 표시
-      marker.setMap(map);
+    });
 }  
