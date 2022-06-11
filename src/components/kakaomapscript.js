39bedb9ad1743261c7da147914600efa156e2c20
LLTae2 <taehyun5820@gmail.com>
2022-06-11 21:40:45 +0900

    iwcontent 변수값 안에 데이터값 전달이 안됨

--- src/components/kakaomapscript.js
+++ src/components/kakaomapscript.js
@@ -1,8 +1,6 @@
 /*global kakao */
 import markerData from "./markerData";
 
-// const { kakao } = window;
-
 export default function KakaoMapScript() {
   const container = document.getElementById('myMap');
 
@@ -16,40 +14,42 @@ export default function KakaoMapScript() {
     var marker = new kakao.maps.Marker({
       //마커가 표시 될 지도
       map: map,
-      position: new kakao.maps.LatLng(el.lat, el.lng),
+      position: new kakao.maps.LatLng(el.lat, el.lng), //생성될 마커의 위도와 경도 설정
     });
+
+    var iwcontent = '<div><h4>{el.title}</h4><img src={el.image} alt="사진"/></div>' //이곳에서 오류 발생!
+      
     // 마커에 표시할 인포윈도우를 생성함
     var infowindow = new kakao.maps.InfoWindow({
-      content: [
-        el.title, // 인포윈도우에 표시할 내용
-        el.src
-      ]
+      content: iwcontent, // 인포윈도우에 표시할 내용
     });
+
+    
     // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록
     // 이벤트 리스너로는 클로저를 만들어 등록
     // 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됨
     kakao.maps.event.addListener(
       marker,
-      "mouseover",
+      "mouseover", //마우스를 올려둘 때 이벤트
       makeOverListener(map, marker, infowindow),
     );
     kakao.maps.event.addListener(
       marker,
-      "mouseout",
+      "mouseout", //마우스가 떠날 때 이벤트
       makeOutListener(infowindow)
     );
     
-    })
+  })
   // 인포윈도우를 표시하는 클로저를 만드는 함수
-    function makeOverListener(map, marker, infowindow) {
-      return function () {
-        infowindow.open(map, marker);
-      };
-    }
-    // 인포윈도우를 닫는 클로저를 만드는 함수
-    function makeOutListener(infowindow) {
-      return function () {
-        infowindow.close();
-      };
-    }
+  function makeOverListener(map, marker, infowindow) {
+    return function () {
+      infowindow.open(map, marker);
+    };
+  }
+  // 인포윈도우를 닫는 클로저를 만드는 함수
+  function makeOutListener(infowindow) {
+    return function () {
+      infowindow.close();
+    };
+  }
 };
--- src/components/markerData.js
+++ src/components/markerData.js
@@ -3,15 +3,18 @@ export default[
     title: "보개산",
     lat: 35.146658,
     lng: 128.824483,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYMooseEpF0MJhjSZzNb7mS4B1N4zAtX0sgYNWBtYkeA&s",
   },
   {
     title: "칠점산",
     lat: 35.1838628,            
     lng: 128.9557129,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd2qGym2a0vFGiqTww5m6m8sNiK6cPzId_OJJUT_V2nQ&s",
   },
   {
-    title: "수유리우동",
-    lat: 35.104411,
-    lng: 128.842423,
+    title: "성화례향 봉화산",
+    lat: 35.1210371,
+    lng: 128.8707085,
+    image: "https://www.bsgangseo.go.kr/visit/img/sub02/photo_gs8_5.jpg",
   },
 ];
