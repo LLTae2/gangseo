f2787b5d3c0af8c4ea6849ee70202ac099b303b9
LLTae2 <taehyun5820@gmail.com>
2022-06-13 23:56:56 +0900

    infowindow에 사진 띄우고 css 정리

--- src/components/kakaomapscript.js
+++ src/components/kakaomapscript.js
@@ -1,5 +1,5 @@
 /*global kakao */
-import markerData from "./markerData";
+import markerData from "./markerData"; // 임의로 만든 데이터파일 불러오기
 
 export default function KakaoMapScript() {
   const container = document.getElementById('myMap');
@@ -9,7 +9,7 @@ export default function KakaoMapScript() {
     level: 7
   };
   const map = new kakao.maps.Map(container, options);
-  markerData.forEach((el) => {
+  markerData.forEach((el) => {  
     //마커를 생성함
     var marker = new kakao.maps.Marker({
       //마커가 표시 될 지도
@@ -17,27 +17,34 @@ export default function KakaoMapScript() {
       position: new kakao.maps.LatLng(el.lat, el.lng), //생성될 마커의 위도와 경도 설정
     });
 
-    var iwcontent = '<div><h4>{el.title}</h4><img src={el.image} alt="사진"/></div>' //이곳에서 오류 발생!
-      
-    // 마커에 표시할 인포윈도우를 생성함
-    var infowindow = new kakao.maps.InfoWindow({
-      content: iwcontent, // 인포윈도우에 표시할 내용
-    });
 
-    
+
+    // infowindow 내부 형태 정의
+    const iwContent = `
+      <div class="iwBox">
+        <h4 class="iwTitle">${el.title}</h4>
+        <img class="iwImage" src=${el.image} alt="사진"/>
+      </div>
+    `;
+
+    // 마커에 표시할 인포윈도우를 생성함
+    const infowindow = new kakao.maps.InfoWindow({
+      content: iwContent, // 인포윈도우에 표시할 내용
+      removable: true,
+    });
     // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록
     // 이벤트 리스너로는 클로저를 만들어 등록
     // 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됨
     kakao.maps.event.addListener(
       marker,
-      "mouseover", //마우스를 올려둘 때 이벤트
+      "click", //마우스를 올려둘 때 이벤트
       makeOverListener(map, marker, infowindow),
     );
-    kakao.maps.event.addListener(
-      marker,
-      "mouseout", //마우스가 떠날 때 이벤트
-      makeOutListener(infowindow)
-    );
+    // kakao.maps.event.addListener(
+    //   marker,
+    //   "mouseout", //마우스가 떠날 때 이벤트
+    //   makeOutListener(infowindow)
+    // );
     
   })
   // 인포윈도우를 표시하는 클로저를 만드는 함수
@@ -47,9 +54,9 @@ export default function KakaoMapScript() {
     };
   }
   // 인포윈도우를 닫는 클로저를 만드는 함수
-  function makeOutListener(infowindow) {
-    return function () {
-      infowindow.close();
-    };
-  }
+  // function makeOutListener(infowindow) {
+  //   return function () {
+  //     infowindow.close();
+  //   };
+  // }
 };
