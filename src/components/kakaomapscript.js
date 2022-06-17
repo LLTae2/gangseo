318eafbb7e7ad9b65fbb725619625cd183246984
LLTae2 <taehyun5820@gmail.com>
2022-06-17 22:13:49 +0900

    kakaomap marker 이미지 변하는 코드

--- src/components/kakaomapscript.js
+++ src/components/kakaomapscript.js
@@ -10,12 +10,21 @@ export default function KakaoMapScript() {
   };
   const map = new kakao.maps.Map(container, options);
   markerData.forEach((el) => {  
-    //마커를 생성함
-    var marker = new kakao.maps.Marker({
-      //마커가 표시 될 지도
-      map: map,
-      position: new kakao.maps.LatLng(el.lat, el.lng), //생성될 마커의 위도와 경도 설정
+    let imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소   
+    imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기
+    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정
+      
+        // 마커의 이미지정보를 가지고 있는 마커이미지를 생성
+    let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+    markerPosition = new kakao.maps.LatLng(el.lat, el.lng); // 마커가 표시될 위치
+
+    // 마커를 생성합니다
+    let marker = new kakao.maps.Marker({
+      position: markerPosition, 
+      image: markerImage // 마커이미지 설정 
     });
+
+    marker.setMap(map);
 
 
 
